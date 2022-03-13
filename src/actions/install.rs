6aca5defe33f9230388b77bc37c75f6f850d1d47
Victor Boizan <victorboizan@protonmail.com>
2022-03-13 19:06:01 +0100

    move installation in it's own file

---
+++ src/actions/install.rs
@@ -0,0 +1,66 @@
+use std::path::{Path, PathBuf};
+
+use regex::Regex;
+use std::fs::{self, File};
+use std::io::{BufRead, BufReader};
+use std::os::unix;
+
+pub fn modules(path: PathBuf, pretend: bool) -> std::io::Result<()> {
+    let regex = Regex::new(
+        "(?:\\[(?P<header>.*)\\])|(?:\"(?P<source>\\./.*)\"\\s+?=\\s*\"(?P<target>.*)\")",
+    )
+    .unwrap();
+    let module_root = path.clone();
+    let mut module_conf = path.clone();
+    module_conf.push(".module.conf");
+    let config = File::open(module_conf.as_path())?;
+    let reader = BufReader::new(config);
+    let mut in_file_section = false;
+
+    for (index, line) in reader.lines().enumerate() {
+        let line = line.unwrap(); // Ignore errors.
+        let captures = regex.captures(line.as_str());
+        if captures.is_some() {
+            let head = captures.as_ref().unwrap().name("header");
+            let src = captures.as_ref().unwrap().name("source");
+            let targ = captures.as_ref().unwrap().name("target");
+            if head.is_some() {
+                if head.unwrap().as_str() == "files" {
+                    in_file_section = true
+                } else {
+                    in_file_section = false
+                }
+            } else if in_file_section {
+                let source = module_root
+                    .clone()
+                    .join(shellexpand::full(src.unwrap().as_str()).unwrap().as_ref())
+                    .canonicalize()
+                    .unwrap();
+                let target_path = shellexpand::full(targ.unwrap().as_str()).unwrap();
+                let target = Path::new(target_path.as_ref());
+
+                #[cfg(debug_assertions)]
+                println!(
+                    "[{}] : source => {} | target => {}",
+                    index,
+                    source.display(),
+                    target.display()
+                );
+                #[cfg(not(debug_assertions))]
+                println!(
+                    "\tsource => {} | target => {}",
+                    source.display(),
+                    target.display()
+                );
+
+                if !pretend {
+                    if !target.parent().unwrap().exists() {
+                        fs::create_dir_all(target.parent().unwrap());
+                    }
+                    unix::fs::symlink(source, target);
+                }
+            }
+        }
+    }
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,12 @@
 use std::env;
 
-use std::path::{Path, PathBuf};
+use std::path::Path;
 
 use git2::Repository;
-use regex::Regex;
-use std::fs::{self, File};
-use std::io::{self, BufRead, BufReader};
-use std::os::unix;
+use std::fs;
+use std::io;
+
+mod actions;
 
 fn main() {
     /*default values for variable*/
@@ -27,7 +27,7 @@ fn main() {
                     args_index += 1;
                 }
                 "--repo" => {
-                    repo = &args[args_index + 1];
+                    url = &args[args_index + 1];
                     args_index += 1;
                 }
 
@@ -66,7 +66,7 @@ fn list_modules(repo_root: &Path, pretend: bool) -> io::Result<()> {
             let path = entry.path();
             if is_module(&path) {
                 println!("{}", path.display());
-                install_module(path, pretend);
+                actions::install::modules(path, pretend);
             } else {
             }
         }
@@ -82,64 +82,4 @@ fn is_module(path: &Path) -> bool {
     }
 
     return false;
-}
-
-fn install_module(path: PathBuf, pretend: bool) -> std::io::Result<()> {
-    let regex = Regex::new(
-        "(?:\\[(?P<header>.*)\\])|(?:\"(?P<source>\\./.*)\"\\s+?=\\s*\"(?P<target>.*)\")",
-    )
-    .unwrap();
-    let module_root = path.clone();
-    let mut module_conf = path.clone();
-    module_conf.push(".module.conf");
-    let config = File::open(module_conf.as_path())?;
-    let reader = BufReader::new(config);
-    let mut in_file_section = false;
-
-    for (index, line) in reader.lines().enumerate() {
-        let line = line.unwrap(); // Ignore errors.
-        let captures = regex.captures(line.as_str());
-        if captures.is_some() {
-            let head = captures.as_ref().unwrap().name("header");
-            let src = captures.as_ref().unwrap().name("source");
-            let targ = captures.as_ref().unwrap().name("target");
-            if head.is_some() {
-                if head.unwrap().as_str() == "files" {
-                    in_file_section = true
-                } else {
-                    in_file_section = false
-                }
-            } else if in_file_section {
-                let source = module_root
-                    .clone()
-                    .join(shellexpand::full(src.unwrap().as_str()).unwrap().as_ref())
-                    .canonicalize()
-                    .unwrap();
-                let target_path = shellexpand::full(targ.unwrap().as_str()).unwrap();
-                let target = Path::new(target_path.as_ref());
-
-                #[cfg(debug_assertions)]
-                println!(
-                    "[{}] : source => {} | target => {}",
-                    index,
-                    source.display(),
-                    target.display()
-                );
-                #[cfg(not(debug_assertions))]
-                println!(
-                    "\tsource => {} | target => {}",
-                    source.display(),
-                    target.display()
-                );
-
-                if !pretend {
-                    if !target.parent().unwrap().exists() {
-                        fs::create_dir_all(target.parent().unwrap());
-                    }
-                    unix::fs::symlink(source, target);
-                }
-            }
-        }
-    }
-    Ok(())
 }
